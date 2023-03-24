fe74f28596852e75fd4d2e4af5f7e6b786d8f8ad
kjuiop <copying_y@naver.com>
2023-03-24 10:54:56 +0900

    [#4] style: areThereDuplicates 문제

---
+++ js-structure-app/ChallengeHomework/areThereDuplicates.js
@@ -0,0 +1,35 @@
+/**
+ * 문제를 이해하라
+ * 문제를 해결할 바탕이 되는 계획들을 세워라
+ * 작성한 내용들을 다 지우고, 다시 시작해서 계획들을 세워라
+ * 해결 가능한 문제들은 풀고, 불가한 문제들은 단순화시켜라
+ * 회고하고, 리팩토링하라
+ */
+
+
+/**
+ * **Frequency Counter / Multiple Pointers - areThereDuplicates**
+ *
+ * Implement a function called, **areThereDuplicates** which accepts a **variable number of arguments,** and **checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+ *
+ * Examples:
+ *
+ * `1. areThereDuplicates(1, 2, 3) // false
+ * 2. areThereDuplicates(1, 2, 2) // true
+ * 3. areThereDuplicates('a', 'b', 'c', 'a') // true`
+ *
+ * **Restrictions:**
+ * Time - O(n)
+ * Space - O(n)
+ *
+ * **Bonus:**
+ * Time - O(n log n)
+ * Space - O(1)
+ */
+
+function areThereDuplicates() {
+
+}
+
+
+
