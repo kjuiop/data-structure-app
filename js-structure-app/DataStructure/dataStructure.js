90efe5556a1011411127379e9efe26fc5a14d774
kjuiop <copying_y@naver.com>
2023-05-21 22:18:07 +0900

    [#18] feat: javascript class 인스턴스 생성

---
+++ js-structure-app/DataStructure/dataStructure.js
@@ -0,0 +1,11 @@
+
+class Student {
+    constructor(firstName, lastName, year) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+    }
+}
+
+console.log('result : ', new Student("Colt", "Steele", 23))
+console.log('result : ', new Student("Blue", "Steele", 25))
