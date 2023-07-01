ee25b3f03961819267a9d7fc1b6757e291cf8563
kjuiop <copying_y@naver.com>
2023-07-01 09:47:52 +0900

    [#26] feat: stack 의 pop

---
+++ js-structure-app/StackAndQueue/stack/pop.js
@@ -0,0 +1,56 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+            this.size++;
+            return;
+        }
+
+        let temp = this.first;
+        this.first = newNode;
+        this.first.next = temp;
+        this.size++;
+    }
+
+    pop() {
+        if (!this.first) {
+            return null;
+        }
+
+        let temp = this.first;
+        if (this.size === 1) {
+            this.first = null;
+            this.last = null;
+            this.size--;
+            return temp.value;
+        }
+
+        this.first = temp.next;
+        this.size--;
+        return temp.value;
+    }
+}
+
+let stack = new Stack();
+stack.push("HELLO")
+stack.push("Friend")
+stack.pop();
+
+console.log("stack : ", stack)
