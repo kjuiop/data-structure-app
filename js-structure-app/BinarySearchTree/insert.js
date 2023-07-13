4eef2005c319d083f05ea05b8eb9804686737ea4
kjuiop <copying_y@naver.com>
2023-07-13 23:13:31 +0900

    [#28] feat: 이진탐색트리 insert

---
+++ js-structure-app/BinarySearchTree/insert.js
@@ -0,0 +1,63 @@
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        let newNode = new Node(value)
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        }
+
+        let current = this.root;
+        while(true) {
+
+            if (value === current.value) {
+                return undefined;
+            }
+
+            if (value < current.value) {
+                if (current.left == null) {
+                    current.left = newNode;
+                    return this;
+                } else {
+                    current = current.left;
+                }
+            } else if (value > current.value) {
+                if (current.right == null) {
+                    current.right = newNode;
+                    return this;
+                } else {
+                    current = current.right;
+                }
+            }
+        }
+
+        return;
+
+    }
+
+    recursiveLeft(value) {
+
+    }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
+
