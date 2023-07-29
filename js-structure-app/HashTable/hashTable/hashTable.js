fdfef02411043061e5e366ab8f4e4572c64362b2
kjuiop <copying_y@naver.com>
2023-07-29 17:30:00 +0900

    [#32] feat: hash table get 기능 추가

--- js-structure-app/HashTable/hashTable/hashTable.js
+++ js-structure-app/HashTable/hashTable/hashTable.js
@@ -24,12 +24,26 @@ class HashTable {
         return index;
     }
 
+    get(key) {
+        let index = this._hash(key);
+        if (this.keyMap[index]) {
+            for (let i=0; i<this.keyMap[index].length; i++) {
+                if (this.keyMap[index][i][0] === key) {
+                    return this.keyMap[index][i][1];
+                }
+            }
+        }
+
+        return undefined;
+    }
+
 }
 
 let ht = new HashTable();
 ht.set("hello world", "good bye!!")
 ht.set("dog", "bulk")
 ht.set("cat", "bye!!")
+ht.set("are we done?", "yes");
 
-console.log("result : ", ht);
+console.log("result : ", ht.get("are we done?"));
 
