96308030b7f59dd1840731458086e73b0738d65a
kjuiop <copying_y@naver.com>
2023-08-09 13:52:05 +0900

    [#34] feat: 두 정점 사이의 간선 생성 로직

--- js-structure-app/Graph/adjacencyList/adjacencyList.js
+++ js-structure-app/Graph/adjacencyList/adjacencyList.js
@@ -7,12 +7,21 @@ class Graph {
         if (!this.adjacencyList[vertex]) {
             this.adjacencyList[vertex] = [];
         }
-
+    }
+    addEdge(v1, v2) {
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
     }
 }
 
 let g = new Graph();
-g.addVertex("sample");
+g.addVertex("Dallas");
+g.addVertex("Seoul");
+g.addVertex("Tokyo");
+
+g.addEdge("Dallas", "Seoul");
+g.addEdge("Seoul", "Tokyo");
+
 console.log("result : ", g);
 
 
