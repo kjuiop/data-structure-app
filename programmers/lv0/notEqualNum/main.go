8c63a51d65a16816509886e17cb9545ca3a09d35
kjuiop <copying_y@naver.com>
2023-09-22 08:34:34 +0900

    [#39] feat: notEqualnum 문제

---
+++ programmers/lv0/notEqualNum/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("expected : [1,3,0,1], actual : ", solution([]int{1, 1, 3, 3, 0, 1, 1}))
+	fmt.Println("expected : [4,3], actual : ", solution([]int{4, 4, 4, 3, 3}))
+}
+
+func solution(nums []int) []int {
+	return nil
+}
